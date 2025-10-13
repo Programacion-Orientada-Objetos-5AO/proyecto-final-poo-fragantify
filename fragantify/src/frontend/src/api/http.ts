6f5c00b1b24d6144bba348b1fc918d9651e8e064
Luciano <Lbgonzalez@alumno.huergo.edu.ar>
2025-10-13 18:30:24 -0300

    correccion frontend

--- fragantify/src/frontend/src/api/http.ts
+++ fragantify/src/frontend/src/api/http.ts
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = "http://localhost:8080";
+const DEFAULT_API_BASE_URL = "http://localhost:8081";
 
 export interface ApiRequestOptions extends RequestInit {
   token?: string;
